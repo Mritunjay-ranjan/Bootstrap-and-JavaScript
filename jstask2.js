61a134d0ff3f2a06f9f0cadc5d101810d2822b75
Mritunjay-ranjan <89678656+Mritunjay-ranjan@users.noreply.github.com>
2023-10-16 00:44:12 +0530

    Update jstask2.js

--- jstask2.js
+++ jstask2.js
@@ -1,8 +1,13 @@
-function alphabet_order(str) {
-  const lettersOnly = str.replace(/[^a-zA-Z]/g, '');
-  const sorted = lettersOnly.split('').sort().join('');
-  return sorted;
+
+function order() {
+    let inputElement = document.getElementById("inputString");
+    let string = inputElement.value.trim(); // Trim any leading or trailing spaces
+
+    // Use a regular expression to check if the input contains only letters
+    if (/^[a-zA-Z]+$/.test(string)) {
+        let alpha = string.split("").sort().join("");
+        document.getElementById("output").textContent = alpha;
+    } else {
+        alert("Please enter a valid string containing only letters.");
+    }
 }
-const str = 'web$mast@er';
-const result = alphabet_order(str);
-console.log(result); 
